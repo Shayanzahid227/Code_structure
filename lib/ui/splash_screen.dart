c9aa46efe7b595b3a61994381c64b63316d1862f
Shayanzahid227 <sshayanzahid227@gmail.com>
2024-10-04 16:31:16 +0500

    updated code all

---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen2 extends StatefulWidget {
+  const SplashScreen2({super.key});
+
+  @override
+  State<SplashScreen2> createState() => _SplashScreen2State();
+}
+
+class _SplashScreen2State extends State<SplashScreen2> {
+  init() async {
+    await Future.delayed(const Duration(seconds: 2), () {
+      // navigator and rout mean from one page to another
+      // Navigator.of(context).push(MaterialPageRoute(
+      //   // builder: (context) => const SignUpScreen(),
+      // ));
+    });
+  }
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          // logo to center of splash screen
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Image.asset(
+            //   //  alignment: Alignment.center,
+            //   "$staticAssets/logo.png",
+            //   scale: 3.0,
+            // ),
+            // // change the size of logo box
+            // const SizedBox(
+            //   height: 20,
+            // ),
+            // Image.asset(
+            //   "$staticAssets/Luciuos.png",
+            //   scale: 3.0,
+            // ),
+            // const SizedBox(
+            //   height: 30,
+            // ),
+            // Image.asset(
+            //   "$staticAssets/Beauty Salon.png",
+            //   scale: 3.0,
+            // ),
+            // const SizedBox(
+            //   height: 30,
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}
