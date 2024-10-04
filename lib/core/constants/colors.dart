bf7a664ab4a3304138ed73515c80a363081ab183
Shayanzahid227 <sshayanzahid227@gmail.com>
2024-10-04 16:22:00 +0500

    code structure

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color bgColor = Color(0xffFFFFFF);
+const Color blackColor = Color(0xff000000);
---
+++ lib/core/constants/text_style.dart
@@ -0,0 +1,5 @@
+import 'package:code_structure/core/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+const style25 =
+    TextStyle(fontSize: 25, color: blackColor, fontWeight: FontWeight.w600);
